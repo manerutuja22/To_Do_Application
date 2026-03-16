f0596ff8a09086be3d53e37b85a57085bb954dd2
Rutuja Mane <manerutuja719@gamil.com>
2026-03-16 22:36:02 +0530

    Add CSS and layout

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -13,14 +13,20 @@ def task_list(request):
 
     return render(request, "tasks/list.html", {"tasks": tasks})
 
+from django.shortcuts import render, redirect
+from .models import Task
+
+
 def update_task(request, id):
 
     task = Task.objects.get(id=id)
 
     if request.method == "POST":
         title = request.POST.get("title")
+
         task.title = title
         task.save()
+
         return redirect("/")
 
     return render(request, "tasks/update.html", {"task": task})
